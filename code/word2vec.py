108e4ec80a4ca88cd46a70dbbb56d3558426f69a
abhibha gupta <34295094+abhibha1807@users.noreply.github.com>
2020-01-15 17:27:12 +0530

    Update word2vec.py

--- code/word2vec.py
+++ code/word2vec.py
@@ -11,8 +11,8 @@ class MySentences(object):
 
 
 def main(domain):
-    source = '../preprocessed_data/%s/train.txt' % (domain)
-    model_file = '../preprocessed_data/%s/w2v_embedding' % (domain)
+    source = '/content/drive/My Drive/preprocessed_data/restaurant/train.txt'
+    model_file = '/content/drive/My Drive/preprocessed_data/restaurant/train.txt'
     sentences = MySentences(source)
     model = gensim.models.Word2Vec(sentences, size=200, window=5, min_count=10, workers=4)
     model.save(model_file)
@@ -20,7 +20,7 @@ def main(domain):
 
 print 'Pre-training word embeddings ...'
 main('restaurant')
-main('beer')
+# main('beer')
 
 
 
